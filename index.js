5d2dacd4ca8abb02665c147fb23452e09cb3ebd5
Allen Dolph <allendolph@gmail.com>
2013-12-23 13:03:15 -0800

    Added ability to pipe the file from s3 to a stream

--- index.js
+++ index.js
@@ -38,6 +38,48 @@ Client.prototype.upload = function(localFile, remoteFile, headers) {
   return uploader;
 };
 
+Client.prototype.streamFile = function(remoteFile, stream) {
+    var streamFile = new EventEmitter();
+    var headers;
+    var amountDone = 0;
+    var amountTotal;
+    var knoxDownload = this.knox.getFile(remoteFile, function (err, resp) {
+        if (err) {
+            downloader.emit('error', err);
+        } else if (resp.statusCode === 200 || resp.statusCode === 307) {
+            amountTotal = parseInt(resp.headers['content-length'], 10);
+            headers = resp.headers;
+            //var writeStream = fs.createWriteStream(localFile);
+            //writeStream.on('error', onError);
+            resp.on('error', onError);
+            resp.on('end', onSuccess);
+            resp.on('data', onData);
+            resp.pipe(stream);
+        } else {
+            streamFile.emit('error', new Error("s3 http status code " + resp.statusCode));
+        }
+        function removeListeners() {
+            resp.removeListener('error', onError);
+            resp.removeListener('end', onSuccess);
+        }
+        function onError(err) {
+            removeListeners();
+            streamFile.emit('error', err);
+        }
+        function onSuccess() {
+            removeListeners();
+            stream.end(null, null, function() {
+                streamFile.emit('end', { headers: headers });
+            });
+        }
+        function onData(data) {
+            amountDone += data.length;
+            streamFile.emit('progress', amountDone, amountTotal);
+        }
+    });
+    return streamFile;
+};
+
 Client.prototype.download = function(remoteFile, localFile) {
   var downloader = new EventEmitter();
   var headers;
